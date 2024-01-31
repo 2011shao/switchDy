d03da0a32b1ab8dd42a0a69aea595846db1df61a
2011shao <562797023@qq.com>
2024-01-31 18:37:46 +0800

    提交

--- src/components/superView/AuthView.vue
+++ src/components/superView/AuthView.vue
@@ -6,20 +6,29 @@
 </template>
 
 <script setup>
+import axios from "axios";
 import { onMounted } from "vue";
 onMounted(() => {
   const state = getUrlDataWithName("state");
   if (state) {
     const backDic = getDyCode(state);
+    const code=getUrlDataWithName('code')
     console.log("返回值", backDic);
+    console.log("返回值code", code);
+
   }
 });
+ function getUserInfo(){
+  axios.get('http://170.106.194.62:5001/').then(res=>{
+
+  })
+
+}
 
 function webAuth() {
   const canScope = "user_info,h5.share,fans.check,open.get.ticket";
   let state = base64UrlEncode(encodeURIComponent('{"id":1,"back":"测试"}')); // encode后拼接到授权链接上
-  window.location.href = `${window.location.href}?state=${state}`;
-  // window.location.href = `https://open.douyin.com/platform/oauth/connect/?client_key=awl98juj5xz2ruu9&response_type=code&state=${state}&scope=${canScope}&redirect_uri=${window.location.href}`;
+  window.location.href = `https://open.douyin.com/platform/oauth/connect/?client_key=awl98juj5xz2ruu9&response_type=code&state=${state}&scope=${canScope}&redirect_uri=${window.location.href}`;
 }
 
 function getDyCode(state) {
